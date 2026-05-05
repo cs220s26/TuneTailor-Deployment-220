0e835b8c6550073348836f6cd5b62c3bfc981d16
Ryan.Demarest <Demarestr@moravian.edu>
2026-05-05 09:36:12 -0400

    added back the resetdb command

---
+++ resetdb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+APP_DIR="/home/ec2-user/TuneTailor-Deployment-220"
+REPO_URL="https://github.com/cs220s26/TuneTailor-Deployment-220.git"
+SERVICE_NAME="tunetailorbot"
+
+cd /home/ec2-user
+
+if [ ! -d "$APP_DIR" ]; then
+  git clone "$REPO_URL" "$APP_DIR"
+fi
+
+cd "$APP_DIR"
+
+git pull origin main
+mvn clean package -DskipTests
+
+sudo systemctl restart "$SERVICE_NAME"
+sudo systemctl status "$SERVICE_NAME" --no-pager
+
+echo "Redeployment complete."%
